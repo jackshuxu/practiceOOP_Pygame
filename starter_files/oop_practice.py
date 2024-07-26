b9ce4fb36af6b7cf13d2122db1584dc1ef3fbd4e
jackshuxu <jackxushu77@gmail.com>
2024-07-26 11:57:59 -0400

    first object

--- starter_files/oop_practice.py
+++ starter_files/oop_practice.py
@@ -4,21 +4,31 @@ import os
 
 # place Pygame window at a specific location
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50,50)
-    
+
+class Vehicle:
+    def __init__(self) -> None:
+        self.img_path = "sedan_red.png"
+        self.location = 400, 400
+        self.draw()
+        
+    def draw(self):
+        # load image and set its location
+        self.img = pygame.image.load(self.img_path)
+        self.img_location = self.img.get_rect()
+        self.img_location.center = self.location
+        
 # pygame settings
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 running = True
 
-# load image and set its location
-img = pygame.image.load("sedan_red.png")
-img_location = img.get_rect()
-img_location.center = 400, 400
-
+car = Vehicle()
 # set background colour
 screen.fill("white")
 # place image on the screen
-screen.blit(img, img_location)
+screen.blit(car.img, car.img_location)
+
+car.draw()
 
 # start game loop
 while running:
